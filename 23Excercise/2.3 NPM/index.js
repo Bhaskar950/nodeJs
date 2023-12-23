c68dd004cef7220d6fbac2c08221eff5ebce0889
Bhaskar950 <koilada9.dev@gmail.com>
2023-12-23 15:28:02 +0530

    npm package silly name and superhero

---
+++ 23Excercise/2.3 NPM/index.js
@@ -0,0 +1,14 @@
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+
+import generateName from "sillyName";
+
+var sillyName = generateName();
+console.log(`my name is ${sillyName}`);
+
+//superheroes
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
